f5c9b60f0881823bd7ce0a0352eff5a1ca7d5589
TetiPalii <TetianaPalii9491@gmail.com>
2023-06-30 10:19:49 +0200

    commit

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -8,12 +8,6 @@ import { useEffect, useState } from 'react';
 import css from './App.module.css';
 
 export const App = () => {
-  const contactArr = [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ];
   const storage = () =>
     JSON.parse(window.localStorage.getItem('storageContacts'));
 
